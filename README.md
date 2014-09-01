559b21242f504d24afb68460092753771e7d9099
yiminghe <yiminghe@gmail.com>
2014-09-01 16:31:57 +0800

    add tc

--- README.md
+++ README.md
@@ -2,9 +2,50 @@
 
 common utilities
 
-## example
+[![modulex-util](https://nodei.co/npm/modulex-util.png)](https://npmjs.org/package/modulex-util)
+[![NPM downloads](http://img.shields.io/npm/dm/modulex-util.svg)](https://npmjs.org/package/modulex-util)
+[![Build Status](https://secure.travis-ci.org/kissyteam/modulex-util.png?branch=master)](https://travis-ci.org/kissyteam/modulex-util)
+[![Coverage Status](https://img.shields.io/coveralls/kissyteam/modulex-util.svg)](https://coveralls.io/r/kissyteam/modulex-util?branch=master)
+[![Dependency Status](https://gemnasium.com/kissyteam/modulex-util.png)](https://gemnasium.com/kissyteam/modulex-util)
+[![Bower version](https://badge.fury.io/bo/modulex-util.svg)](http://badge.fury.io/bo/modulex-util)
+
+[![browser support](https://ci.testling.com/kissyteam/modulex-util.png)](https://ci.testling.com/kissyteam/modulex-util)
+
+## api
+
+http://docs.kissyui.com/5.0/api/modules/util.html
+
+## guide
+
+http://docs.kissyui.com/5.0/guides/path/index.html
+
+## use on nodejs
 
 ``` javascript
 var  xutil = require('modulex-util');
 console.log(xutil.clone([1,2]));
 ```
+
+## changelog
+
+https://github.com/kissyteam/modulex-util/milestones
+
+## contribute
+
+### fork and clone
+
+### prepare environment
+```
+npm install bower -g
+npm install
+bower install
+node server
+```
+
+then open http://localhost:8001/tests/runner.html
+
+### open issue
+
+### modify code at lib
+
+### commit and pull request
---
+++ gulpfile.js
@@ -0,0 +1,59 @@
+var gulp = require('gulp');
+var filter = require('gulp-filter');
+var kclean = require('gulp-kclean');
+var modulex = require('gulp-modulex');
+var path = require('path');
+var rename = require('gulp-rename');
+var packageInfo = require('./package.json');
+var src = path.resolve(process.cwd(), 'lib');
+var build = path.resolve(process.cwd(), 'build');
+var clean = require('gulp-clean');
+var uglify = require('gulp-uglify');
+var jshint = require('gulp-jshint');
+var stylish = require('jshint-stylish');
+var jscs = require('gulp-jscs');
+var replace = require('gulp-replace');
+
+gulp.task('lint', function () {
+    return gulp.src('./lib/**/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter(stylish))
+        .pipe(jshint.reporter('fail'))
+        .pipe(jscs());
+});
+
+gulp.task('clean', function () {
+    return gulp.src(build, {
+        read: false
+    }).pipe(clean());
+});
+
+gulp.task('build', ['lint'], function () {
+    return gulp.src('./lib/util.js')
+        .pipe(modulex({
+            modulex: {
+                packages: {
+                    util: {
+                        base: path.resolve(src, 'util')
+                    }
+                }
+            }
+        }))
+        .pipe(kclean({
+            files: [
+                {
+                    src: './lib/util-debug.js',
+                    outputModule: 'util'
+                }
+            ]
+        }))
+        .pipe(replace(/@VERSION@/g, packageInfo.version))
+        .pipe(gulp.dest(build))
+        .pipe(filter('util-debug.js'))
+        .pipe(replace(/@DEBUG@/g, ''))
+        .pipe(uglify())
+        .pipe(rename('util.js'))
+        .pipe(gulp.dest(build));
+});
+
+gulp.task('default', ['build']);
